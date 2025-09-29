8fa367742bd2706122e1ca153aaeca1964eb68a7
goncang1512 <samuderanst@gmail.com>
2025-09-29 21:37:42 +0700

    fix: perbaikan bug untuk authentication

--- src/controller/auth.controller.ts
+++ src/controller/auth.controller.ts
@@ -1,58 +1,155 @@
 import { auth } from "@/lib/auth.js";
 import prisma from "@/lib/prisma-client.js";
+import { APIError } from "better-auth";
 import { Hono } from "hono";
 
 const authApp = new Hono();
 
+authApp.post("/provider", async (c) => {
+  const body = await c.req.json();
+  try {
+    const session = await auth.api.signInSocial({
+      body: {
+        provider: "google",
+      },
+    });
+
+    return c.json({
+      status: true,
+      statusCode: 200,
+      message: "Success login provider",
+      result: "session",
+    });
+  } catch (error) {
+    if (error instanceof APIError) {
+      return c.json(
+        {
+          status: false,
+          statusCode: error.body?.code,
+          message: error.message,
+          result: null,
+        },
+        (error.body?.code as any) ?? 401
+      );
+    }
+
+    return c.json(
+      {
+        status: false,
+        statusCode: 500,
+        message: "Internal Server Error",
+        result: error,
+      },
+      500
+    );
+  }
+});
+
 authApp.post("/login", async (c) => {
   const body = await c.req.json();
 
-  const session = await auth.api.signInEmail({
-    body: {
-      email: body.email,
-      password: body.email,
-    },
-  });
+  try {
+    const session = await auth.api.signInEmail({
+      body: {
+        email: body.email,
+        password: body.password,
+      },
+    });
 
-  return c.json(
-    {
-      status: true,
-      statusCode: 200,
-      message: "Success login",
-      result: session,
-    },
-    200
-  );
+    return c.json(
+      {
+        status: true,
+        statusCode: 200,
+        message: "Success login",
+        result: { ...session },
+      },
+      200
+    );
+  } catch (error) {
+    if (error instanceof APIError) {
+      return c.json(
+        {
+          status: false,
+          statusCode: error.body?.code,
+          message: error.message,
+          result: null,
+        },
+        (error.body?.code as any) ?? 401
+      );
+    }
+
+    return c.json(
+      {
+        status: false,
+        statusCode: 500,
+        message: "Internal Server Error",
+        result: error,
+      },
+      500
+    );
+  }
 });
 
 authApp.post("/register", async (c) => {
   const body = await c.req.json();
 
-  const session = await auth.api.signUpEmail({
-    body: {
-      email: body.email,
-      name: body.email,
-      password: body.password,
-    },
-  });
+  try {
+    if (body.password !== body.confirmPassword) {
+      throw new APIError(422, {
+        code: "422",
+        message: "Invalid Password and Confirmation Password",
+      });
+    }
 
-  return c.json(
-    {
-      status: true,
-      statusCode: 201,
-      message: "Success register user",
-      result: session,
-    },
-    201
-  );
+    const session = await auth.api.signUpEmail({
+      body: {
+        email: body.email,
+        name: body.name,
+        password: body.password,
+      },
+    });
+
+    return c.json(
+      {
+        status: true,
+        statusCode: 200,
+        message: "Success login",
+        result: { ...session },
+      },
+      200
+    );
+  } catch (error) {
+    if (error instanceof APIError) {
+      return c.json(
+        {
+          status: false,
+          statusCode: error.body?.code,
+          message: error.message,
+          result: null,
+        },
+        (error.body?.code as any) ?? 401
+      );
+    }
+
+    return c.json(
+      {
+        status: false,
+        statusCode: 500,
+        message: "Internal Server Error",
+        result: error,
+      },
+      500
+    );
+  }
 });
 
 authApp.get("/session", async (c) => {
-  const cookieHeader = c.req.header("cookie") || "";
+  const authHeader = c.req.header("authorization") || "";
+  const token = authHeader.replace(/^Bearer\s+/i, "");
 
   const session = await prisma.session.findFirst({
     where: {
-      token: cookieHeader,
+      token: token,
     },
     select: {
       id: true,
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,9 +3,18 @@ import { prismaAdapter } from "better-auth/adapters/prisma";
 import prisma from "./prisma-client.js";
 
 export const auth = betterAuth({
+  baseURL: process.env.BETTER_AUTH_URL,
   database: prismaAdapter(prisma, {
     provider: "postgresql",
   }),
+  socialProviders: {
+    google: {
+      prompt: "select_account",
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      redirectUrl: "myapp://callback",
+    },
+  },
   emailAndPassword: {
     enabled: true,
   },
