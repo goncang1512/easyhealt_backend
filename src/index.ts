615497cabcd9d5666c2c12927a6cfd9a90262668
goncang1512 <samuderanst@gmail.com>
2025-10-04 21:44:47 +0700

    fix: bug in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,9 @@
 import { serve } from "@hono/node-server";
-import { Hono } from "hono";
 import routes from "./routes/routes.js";
-
-const app = new Hono();
-app.route("/", routes);
 
 serve(
   {
-    fetch: app.fetch,
+    fetch: routes.fetch,
     port: 3000,
   },
   (info) => {
