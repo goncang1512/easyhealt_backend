a81285007317135220a18869beb4b6a4da61245b
goncang1512 <samuderanst@gmail.com>
2025-10-05 11:42:58 +0700

    fix: bug

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,7 @@
 import { defineConfig } from "vite";
 import honoBuild from "@hono/vite-build/node";
-import path from "path";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [honoBuild()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"), // <-- ini penting
-    },
-  },
-  build: {
-    outDir: "dist",
-    target: "esnext",
-    sourcemap: true,
-    rollupOptions: {
-      input: "./src/index.ts",
-      output: {
-        format: "esm",
-        entryFileNames: "index.js",
-      },
-      external: [
-        "@prisma/client", // Tambahkan Prisma ke `external`
-      ],
-    },
-  },
+  plugins: [honoBuild(), tsconfigPaths()],
 });
