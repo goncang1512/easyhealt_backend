66ca7c06a77fdeb81916d2c5d3fa18669f4573c4
goncang1512 <samuderanst@gmail.com>
2025-12-17 11:35:46 +0700

    fix: di validasi body data

--- src/middleware/validator/booking.schema.ts
+++ src/middleware/validator/booking.schema.ts
@@ -8,8 +8,19 @@ export const createBookingSchema = z.object({
     .regex(/^[0-9]+$/, { message: "Nomor telepon hanya boleh berisi angka" })
     .min(8, { message: "Nomor telepon minimal 8 digit" })
     .max(15, { message: "Nomor telepon maksimal 15 digit" }),
-  bookDate: z.string().min(1, { message: "Tanggal booking wajib diisi" }),
-  bookTime: z.string().min(1, { message: "Waktu booking wajib diisi" }),
+  bookDate: z
+    .string()
+    .min(1, { message: "Tanggal booking wajib diisi" })
+    .transform((val) => {
+      const [day, month, year] = val.split("/");
+      return `${year}-${month}-${day}`; // hasil jadi YYYY-MM-DD
+    }),
+  bookTime: z
+    .string()
+    .min(1, { message: "Waktu booking wajib diisi" })
+    .regex(/^(0?[1-9]|1[0-2]):[0-5][0-9]\s?(AM|PM)$/i, {
+      message: "Format waktu harus hh:mm AM/PM",
+    }),
   note: z.string().optional(),
   hospitalId: z.string().min(31, { message: "ID rumah sakit wajib diisi" }),
   docterId: z.string().min(31, { message: "ID dokter wajib diisi" }),
