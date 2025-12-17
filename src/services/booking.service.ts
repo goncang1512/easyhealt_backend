47d21925f5932aa670d35c5d01ea2faad818f4ca
goncang1512 <samuderanst@gmail.com>
2025-12-17 11:44:21 +0700

    fix

--- src/services/booking.service.ts
+++ src/services/booking.service.ts
@@ -6,25 +6,6 @@ import notifStore from "./firestore/notification.store.js";
 
 const bookingService = {
   createBooking: async (body: BookingSchemaT.CreateBookingInput) => {
-    // ✅ Normalisasi waktu: pastikan ada spasi sebelum AM/PM
-    const normalizedTime = body.bookTime.replace(/(AM|PM)$/i, " $1");
-
-    const combined = `${body.bookDate} ${normalizedTime}`;
-    // contoh hasil: 2025-12-15 12:00 AM
-
-    const parsedDate = parse(combined, "yyyy-MM-dd h:mm a", new Date());
-
-    if (isNaN(parsedDate.getTime())) {
-      console.error("INVALID DATE INPUT:", {
-        bookDate: body.bookDate,
-        bookTime: body.bookTime,
-        combined,
-      });
-      throw new Error("Invalid date format");
-    }
-
-    const dateTimeISO = parsedDate.toISOString();
-
     const docter = await prisma.docter.findFirst({
       where: {
         id: body.docterId,
@@ -44,12 +25,12 @@ const bookingService = {
         id: generateId(32),
         name: body.name,
         bookDate: body.bookDate,
-        bookTime: normalizedTime, // ✅ simpan versi yang sudah rapi
+        bookTime: body.bookTime, // ✅ simpan versi yang sudah rapi
         noPhone: body.noPhone,
         docterId: body.docterId,
         hospitalId: body.hospitalId,
         note: body.note,
-        bookDateTime: dateTimeISO,
+        bookDateTime: `${body.bookDate} ${body.bookTime}`,
         status: "confirm",
         userId: body.userId,
         bookingNumber: `${docter?.prefix}-${(docter?.booking.length ?? 0) + 1}`,
